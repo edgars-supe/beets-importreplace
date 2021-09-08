7cf8846107f120f3a6755050ea68af71bb47f8d5
Edgars Supe <edgars.supe@gmail.com>
2021-09-08 20:08:13 +0300

    Add setup.py configuration

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='beets-importreplace',
+    version='0.1',
+    description='beets plugin to perform replacements on import metadata',
+    long_description=open('README.md').read(),
+    author='Edgars Supe',
+    author_email='',
+    url='https://github.com/edgars-supe/beets-importreplace',
+    license='MIT',
+    platforms='ALL',
+    packages=['beetsplug'],
+    install_requires=['beets>=1.4.7'],
+)
