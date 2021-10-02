4f27c1c466e651fce70b1c9676316d8deca6376f
Edgars Supe <edgars.supe@gmail.com>
2021-10-02 12:21:38 +0300

    Add ability to specify per-field replacements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ImportReplace
 
 A plugin for [beets](https://github.com/beetbox/beets) to perform regex
-replacements during import. 
+replacements during import.
 
 It works on metadata in `AlbumInfo` and `TrackInfo` objects as they come in.
 Running many replacements (or on many fields) might negatively impact import
@@ -10,35 +10,73 @@ performance.
 A good usecase for this plugin is replacing the Unicode apostrophes, quotation
 marks and hyphens used by MusicBrainz with their ASCII counterparts. The
 built-in [`replace`](https://beets.readthedocs.io/en/stable/reference/config.html#replace)
-configuration won't work for this, because it only acts on filenames. 
+configuration won't work for this, because it only acts on filenames.
+
+## Installation
+
+Install the plugin using `pip`:
+
+```shell
+pip install git+git://github.com/edgars-supe/beets-importreplace.git
+```
+
+Then, [configure](#configuration) the plugin in your
+[`config.yaml`](https://beets.readthedocs.io/en/latest/plugins/index.html) file.
 
 ## Configuration
 
-To configure the plugin, add a `importreplace:` configuration block in your
-beets `config.yaml` file. This example configuration replaces Unicode
-apostrophes, quotes and hyphen used by MusicBrainz with their ASCII
-counterparts.
+First, add `importreplace` to your list of enabled plugins.
+
+```yaml
+plugins: importreplace
+```
+
+Then, add a `importreplace:` configuration block in your beets `config.yaml`
+file. Add one or more replacement configurations under `replacements:` This
+example configuration replaces Unicode apostrophes, quotes and hyphen used by
+MusicBrainz with their ASCII counterparts.
 
 ```yaml
 importreplace:
-    item_fields: title artist artist_sort artist_credit
-    album_fields: album artist artist_sort artist_credit
-    replace:
+  replacements:
+    - item_fields: title artist artist_sort artist_credit
+      album_fields: album artist artist_sort artist_credit
+      replace:
         '[\u2018-\u201B]': ''''
         '[\u201C-\u201F]': '"'
         '[\u2010-\u2015]': '-'
 ```
+
 * `item_fields` is a set of fields to run replacements on for items
 * `album_fields` is a set of fields to run replacements on for albums
 * `replace` is a set of regex/replacement pairs
 
 That's about it.
 
-## Installation
+You can add more replacement configurations for different fields. For example,
+to replace certain strings only for tracks, but different strings only for
+albums.
 
-Install the plugin using `pip`:
-```shell
-pip install git+git://github.com/edgars-supe/beets-importreplace.git
+```yaml
+importreplace:
+  replacements:
+    - item_fields: title
+      replace:
+        'The': 'That'
+    - item_fields: album
+      album_fields: album
+      replace:
+        'Foo': 'Bar'
 ```
-Then, [configure](#configuration) the plugin in your
-[`config.yaml`](https://beets.readthedocs.io/en/latest/plugins/index.html) file.
+
+This config will convert
+
+```
+Artist - The Fighters of Foo/Artist - The Foo Song
+```
+
+into
+
+```
+Artist - The Fighters of Bar/Artist - That Foo Song
+```
--- tests/test_importreplace.py
+++ tests/test_importreplace.py
@@ -1,12 +1,19 @@
 import unittest
 
 import beets as beets
+import confuse as confuse
 from beets.autotag.hooks import AlbumInfo
 from beets.autotag.hooks import TrackInfo
 from beetsplug.importreplace import ImportReplace
 
 
 class ImportReplaceTest(unittest.TestCase):
+
+    def setUp(self):
+        beets.config['importreplace'] = {'replacements': []}
+
+    def tearDown(self):
+        beets.config['importreplace'] = {'replacements': []}
 
     def _create_track_info(self, title: str = None, artist: str = None,
                            artist_sort: str = None, artist_credit: str = None):
@@ -19,38 +26,116 @@ class ImportReplaceTest(unittest.TestCase):
         return AlbumInfo(tracks=tracks or [], album=album, artist=artist,
                          artist_sort=artist_sort, artist_credit=artist_credit)
 
-    def _set_config(self, item_fields: [str], album_fields: [str],
-                    replace: {str: str}):
-        beets.config['importreplace']['item_fields'] = item_fields
-        beets.config['importreplace']['album_fields'] = album_fields
-        beets.config['importreplace']['replace'] = replace
+    def _add_replacement(self, item_fields: [str] = None,
+                         album_fields: [str] = None,
+                         replace: {str: str} = None):
+        replacement = {}
+        if item_fields:
+            replacement['item_fields'] = item_fields
+        if album_fields:
+            replacement['album_fields'] = album_fields
+        if replace:
+            replacement['replace'] = replace
+
+        beets.config['importreplace']['replacements'] \
+            .get(list) \
+            .append(replacement)
 
     def test_replaces_only_config_fields(self):
         """Check if plugin replaces text in only the specified fields"""
-        self._set_config(item_fields=['title'], album_fields=['album'],
-                         replace={'a': 'x'})
-        tracks = [self._create_track_info(title='Fao', artist='Bar')]
-        album_info = self._create_album_info(tracks=tracks, album='albumTest',
-                                             artist='Bar')
+        self._add_replacement(item_fields=['title'], album_fields=['album'],
+                              replace={'The': 'A'})
+        tracks = [self._create_track_info(title='The Piece', artist='The Dude')]
+        album_info = self._create_album_info(tracks=tracks, album='The Album',
+                                             artist='The Dude')
         subject = ImportReplace()
         subject._albuminfo_received(album_info)
-        self.assertEqual(album_info.album, 'xlbumTest')
-        self.assertEqual(album_info.artist, 'Bar')
-        self.assertEqual(album_info.tracks[0].title, 'Fxo')
-        self.assertEqual(album_info.tracks[0].artist, 'Bar')
+        self.assertEqual(album_info.album, 'A Album')
+        self.assertEqual(album_info.artist, 'The Dude')
+        self.assertEqual(album_info.tracks[0].title, 'A Piece')
+        self.assertEqual(album_info.tracks[0].artist, 'The Dude')
+
+    def test_replaces_only_config_fields_multiple(self):
+        """Check if plugin replaces text in only the specified fields when
+        multiple replacements given."""
+        self._add_replacement(item_fields=['title'], album_fields=['album'],
+                              replace={'The': 'A'})
+        self._add_replacement(item_fields=['artist'], album_fields=['artist'],
+                              replace={'This': 'That'})
+        tracks = [self._create_track_info(title='The Piece', artist='The This')]
+        album_info = self._create_album_info(tracks=tracks,
+                                             album='The Album',
+                                             artist='The This')
+        subject = ImportReplace()
+        subject._albuminfo_received(album_info)
+        self.assertEqual(album_info.album, 'A Album')
+        self.assertEqual(album_info.artist, 'The That')
+        self.assertEqual(album_info.tracks[0].title, 'A Piece')
+        self.assertEqual(album_info.tracks[0].artist, 'The That')
 
     def test_handles_empty_fields(self):
         """Verify that plugin works when field marked for replacement
          is absent"""
-        self._set_config(item_fields=['title', 'artist'],
-                         album_fields=['album', 'artist'],
-                         replace={'a': 'x'})
-        tracks = [self._create_track_info(title='Fao', artist=None)]
-        album_info = self._create_album_info(tracks=tracks, album='albumTest',
+        self._add_replacement(item_fields=['title', 'artist'],
+                              album_fields=['album', 'artist'],
+                              replace={'This': 'That'})
+        tracks = [self._create_track_info(title='This Piece', artist=None)]
+        album_info = self._create_album_info(tracks=tracks, album='This Album',
                                              artist=None)
         subject = ImportReplace()
         subject._albuminfo_received(album_info)
-        self.assertEqual(album_info.album, 'xlbumTest')
+        self.assertEqual(album_info.album, 'That Album')
         self.assertEqual(album_info.artist, None)
-        self.assertEqual(album_info.tracks[0].title, 'Fxo')
+        self.assertEqual(album_info.tracks[0].title, 'That Piece')
         self.assertEqual(album_info.tracks[0].artist, None)
+
+    def test_replaces_in_order(self):
+        """Verify taht the plugin replaces fields in the order given in the
+        config."""
+        self._add_replacement(item_fields=['title'], album_fields=['album'],
+                              replace={'The': 'This'})
+        self._add_replacement(item_fields=['title'], album_fields=['album'],
+                              replace={'This': 'That'})
+        tracks = [self._create_track_info(title='The Piece', artist='The Dude')]
+        album_info = self._create_album_info(tracks=tracks, album='The Album',
+                                             artist='The Dude')
+        subject = ImportReplace()
+        subject._albuminfo_received(album_info)
+        self.assertEqual(album_info.album, 'That Album')
+        self.assertEqual(album_info.artist, 'The Dude')
+        self.assertEqual(album_info.tracks[0].title, 'That Piece')
+        self.assertEqual(album_info.tracks[0].artist, 'The Dude')
+
+    def test_incorrect_field(self):
+        """Verify the plugin works when a non-existent field is specified."""
+        self._add_replacement(item_fields=['asdf'], album_fields=['asdf'],
+                              replace={'This': 'That'})
+        tracks = [self._create_track_info(title='The Piece', artist='The Dude')]
+        album_info = self._create_album_info(tracks=tracks, album='The Album',
+                                             artist='The Dude')
+        subject = ImportReplace()
+        subject._albuminfo_received(album_info)
+        self.assertEqual(album_info.album, 'The Album')
+        self.assertEqual(album_info.artist, 'The Dude')
+        self.assertEqual(album_info.tracks[0].title, 'The Piece')
+        self.assertEqual(album_info.tracks[0].artist, 'The Dude')
+
+    def test_no_fields(self):
+        """Verify the plugin works when item_fields or album_fields not
+        given."""
+        """Check if plugin replaces text in only the specified fields when
+        multiple replacements given."""
+        self._add_replacement(item_fields=['title'],
+                              replace={'The': 'A'})
+        self._add_replacement(album_fields=['artist'],
+                              replace={'This': 'That'})
+        tracks = [self._create_track_info(title='The Piece', artist='The This')]
+        album_info = self._create_album_info(tracks=tracks,
+                                             album='The Album',
+                                             artist='The This')
+        subject = ImportReplace()
+        subject._albuminfo_received(album_info)
+        self.assertEqual(album_info.album, 'The Album')
+        self.assertEqual(album_info.artist, 'The That')
+        self.assertEqual(album_info.tracks[0].title, 'A Piece')
+        self.assertEqual(album_info.tracks[0].artist, 'The This')
